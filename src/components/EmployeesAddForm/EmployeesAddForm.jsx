91fa3d5b0e020bba92046f309e0c15bf378eac7f
devmaffi <devmaffi550@gmail.com>
2022-06-09 12:31:51 +0300

    Transform EmployeesAddForm inputs into controlled elements

--- src/components/EmployeesAddForm/EmployeesAddForm.jsx
+++ src/components/EmployeesAddForm/EmployeesAddForm.jsx
@@ -1,22 +1,53 @@
+import { Component } from 'react'
+
 import { AppInput, Button } from '../common'
 
 import './EmployeesAddForm.css'
 
-function EmployeesAddForm() {
-  return (
-    <div className="app-add-form">
-      <h3>Add new employee</h3>
-      <form className="add-form d-flex">
-        <AppInput className="new-post-label" placeholder="What's his name?" />
-        <AppInput
-          type="number"
-          className="new-post-label"
-          placeholder="Salary in $?"
-        />
-        <Button type="submit">Add</Button>
-      </form>
-    </div>
-  )
+class EmployeesAddForm extends Component {
+  state = {
+    data: {
+      name: '',
+      salary: '',
+    },
+  }
+
+  onValueChange = e => {
+    const { currentTarget: input } = e
+
+    const data = { ...this.state.data }
+    data[input.name] = input.value
+
+    this.setState({ data })
+  }
+
+  #renderInput = (name, placeholder, type) => {
+    type = type ?? 'text'
+
+    return (
+      <AppInput
+        type={type}
+        name={name}
+        className="new-post-label"
+        placeholder={placeholder}
+        value={this.state.data[name]}
+        onChange={this.onValueChange}
+      />
+    )
+  }
+
+  render() {
+    return (
+      <div className="app-add-form">
+        <h3>Add new employee</h3>
+        <form className="add-form d-flex">
+          {this.#renderInput('name', "What's his name?")}
+          {this.#renderInput('salary', 'Salary in $?', 'number')}
+          <Button type="submit">Add</Button>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default EmployeesAddForm
