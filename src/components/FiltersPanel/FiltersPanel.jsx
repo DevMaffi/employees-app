86b310fd4ce48f85cc73dab8da7988b3f8c74bdc
devmaffi <devmaffi550@gmail.com>
2022-06-11 11:55:06 +0300

    Add styled-components to FiltersPanel

--- src/components/FiltersPanel/FiltersPanel.jsx
+++ src/components/FiltersPanel/FiltersPanel.jsx
@@ -1,19 +1,28 @@
+import styled from 'styled-components'
+
 import EmployeesFilter from '../EmployeesFilter'
 
 import SearchPanel from '../common/SearchPanel'
 
-import './FiltersPanel.css'
+const StyledFiltersPanel = styled.div`
+  margin-top: 30px;
+  padding: 25px;
+  border-radius: 4px;
+  background-color: #3d5a80;
+  box-shadow: 15px 15px 30px rgba(0, 0, 0, 0.15);
+  color: white;
+`
 
 function FiltersPanel({ queryString, onSearch, filters, onFilterChange }) {
   return (
-    <div className="filters-panel">
+    <StyledFiltersPanel>
       <SearchPanel
         placeholder="Find an employee"
         queryString={queryString}
         onSearch={onSearch}
       />
       <EmployeesFilter filters={filters} onFilterChange={onFilterChange} />
-    </div>
+    </StyledFiltersPanel>
   )
 }
 
