de1746896ccf9b192b1faa3028ca58f2405ee108
devmaffi <devmaffi550@gmail.com>
2022-06-11 11:52:03 +0300

    Migrate Header css styles to styled-components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,20 @@
-import './Header.css'
+import styled from 'styled-components'
+
+const AppInfo = styled.div`
+  padding: 25px;
+  border-radius: 4px;
+  background-color: #3d5a80;
+  box-shadow: 15px 15px 30px rgba(0, 0, 0, 0.15);
+  color: white;
+`
 
 function Header({ total, withPremium }) {
   return (
-    <div className="app-info">
+    <AppInfo>
       <h1>Employees accountant</h1>
       <h2>Total number of employees - {total}</h2>
       <h2>Will receive the increase - {withPremium}</h2>
-    </div>
+    </AppInfo>
   )
 }
 
