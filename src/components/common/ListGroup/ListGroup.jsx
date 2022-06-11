b43a2a9f0993618ae028c31cb6b609162d336ddb
devmaffi <devmaffi550@gmail.com>
2022-06-11 12:18:44 +0300

    Apply styled-components on ListGroup

--- src/components/common/ListGroup/ListGroup.jsx
+++ src/components/common/ListGroup/ListGroup.jsx
@@ -1,8 +1,15 @@
-import './ListGroup.css'
+import styled from 'styled-components'
 
-function ListGroup({ data, component: Component, ...componentProps }) {
+function ListGroup({
+  data,
+  className,
+  component: Component,
+  ...componentProps
+}) {
+  const classNames = `list-group ${className}`
+
   return (
-    <ul className="list-group list">
+    <ul className={classNames}>
       {data.map(item => {
         const { id } = item
         return <Component key={id} item={item} {...componentProps} />
@@ -11,4 +18,12 @@ function ListGroup({ data, component: Component, ...componentProps }) {
   )
 }
 
-export default ListGroup
+const StyledList = styled(ListGroup)`
+  margin-top: 30px;
+  background-color: #3d5a80;
+  border-radius: 4px;
+  box-shadow: 15px 15px 30px rgba(0, 0, 0, 0.15);
+  color: white;
+`
+
+export default StyledList
